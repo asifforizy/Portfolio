195e499759d6a8d2bd24fc531a0aa04a08907ffc
Asif Forizy <asifforizy@gmail.com>
2026-06-17 05:03:37 +0600

    set dark veil as bg

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import Navbar from '../Components/Navbar';
-import { Outlet } from 'react-router';
-import Footer from '../Components/Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import DarkVeil from "../Components/DarkVeil";
 
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className="relative min-h-screen">
+      {/* Background */}
+      <div className="fixed inset-0 -z-10">
+        <DarkVeil
+          hueShift={0}
+          noiseIntensity={0}
+          scanlineIntensity={0}
+          speed={0.5}
+          scanlineFrequency={0}
+          warpAmount={0}
+        />
+      </div>
+
+      {/* Content */}
+      <div className="relative z-10">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Root;
